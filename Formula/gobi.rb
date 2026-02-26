e94f5a58a4788e10f1a9a3106ec867485bd8789d
gpminsuk <gpminsuk@gmail.com>
2026-02-25 19:15:10 -0800

    Add gobi formula

---
+++ Formula/gobi.rb
@@ -0,0 +1,18 @@
+class Gobi < Formula
+  desc "CLI client for the Gobi collaborative knowledge platform"
+  homepage "https://github.com/gobi-ai/gobi-cli"
+  url "https://registry.npmjs.org/@gobi-ai/cli/-/cli-0.1.0.tgz"
+  # sha256 "UPDATE_AFTER_FIRST_NPM_PUBLISH"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gobi --version")
+  end
+end
