cb70b480ca9a1eba4b3171b6e70747d808abac01
gpminsuk <gpminsuk@gmail.com>
2026-06-23 19:57:28 -0700

    Update gobi formula to v2.0.33

--- Formula/gobi.rb
+++ Formula/gobi.rb
@@ -1,8 +1,8 @@
 class Gobi < Formula
   desc "CLI client for the Gobi collaborative knowledge platform"
   homepage "https://github.com/gobi-ai/gobi-cli"
-  url "https://registry.npmjs.org/@gobi-ai/cli/-/cli-2.0.32.tgz"
-  sha256 "89745ad861ad89fdac4654ad3353bf83a3c97bb200af7abd5dbb255cb8d535bb"
+  url "https://registry.npmjs.org/@gobi-ai/cli/-/cli-2.0.33.tgz"
+  sha256 "ca497d1999be6cb76530eb9073bd321e8a03493c25312c6fe605ccb07c9fca2f"
   license "MIT"
 
   depends_on "node"
